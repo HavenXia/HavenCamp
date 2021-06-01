3d384cc70e6866c66380fe86f8870f7cb33c931a
Haoyu Xia <55463354+HavenXia@users.noreply.github.com>
2021-06-01 14:50:22 +0800

    add update operation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+// require the method-override
+const methodOverride = require('method-override')
+
 // require the campground model 
 const Campground = require('./models/campground')
 
@@ -25,20 +28,73 @@ db.once("open", () => {
 // set ejs path
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+// parse method
+app.use(express.urlencoded({ extended: true }));
+// override configuration
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 // index operation: show all campgrounds
-app.get('/campground', async (req, res) => {
-
+app.get('/campgrounds', async (req, res) => {
     // get an array of all campgrounds
     const campgrounds = await Campground.find({});
-    // render the array to ejs
-    res.render('campgrounds/index', campgrounds);
+    // render the array to ejs (views/campgrounds/index.ejs)
+    res.render('campgrounds/index', { campgrounds });
 })
+
+// new form operation
+// must before show operation to make sure the order is right
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+// add the new documents into db
+app.post('/campgrounds', async (req, res) => {
+    // cast to Campground
+    // 这里加上campground是因为ejs里面写的是campground[title]
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    // redirect with id
+    res.redirect(`/campgrounds/${newCampground._id}`)
+})
+
+// show operation
+app.get('/campgrounds/:id', async (req, res) => {
+    // get the id from input
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    // render the found campground to ejs
+    res.render('campgrounds/show', { campground })
+})
+
+// edit form operation, the order matters
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    // get the id from input
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    // render the found campground to ejs
+    res.render('campgrounds/edit', { campground })
+})
+
+// put method to update campground
+app.put('/campgrounds/:id', async (req, res) => {
+    // get id
+    const { id } = req.params;
+    // update the dpcument with 
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// app.put('/campgrounds/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+//     res.redirect(`/campgrounds/${campground._id}`)
+// });
+
+
 
 app.listen(3000, () => {
     console.log("listening")
