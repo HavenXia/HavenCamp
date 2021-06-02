d68f765c7a28a5b8e824a4c60cc3c93b8e9304bb
Haoyu Xia <55463354+HavenXia@users.noreply.github.com>
2021-06-02 10:11:51 +0800

    add delete function

--- app.js
+++ app.js
@@ -83,18 +83,16 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 app.put('/campgrounds/:id', async (req, res) => {
     // get id
     const { id } = req.params;
-    // update the dpcument with 
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    // update the dpcument with req.body.campground (是个object)
+    const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
-// app.put('/campgrounds/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-//     res.redirect(`/campgrounds/${campground._id}`)
-// });
-
-
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect(`/campgrounds`)
+})
 
 app.listen(3000, () => {
     console.log("listening")
